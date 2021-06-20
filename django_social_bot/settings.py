c32a2b403d0839892273a8c7130924f1e257a54d
mrprocs <mrprocs@gmail.com>
2021-06-20 21:52:22 +0430

    Generate social django web site for auto follow, like and write comment in social networks

--- django_social_bot/settings.py
+++ django_social_bot/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_social_bot_instagram_register.apps.DjangoSocialBotInstagramRegisterConfig',
+    'django_social_bot_instagram_register.django_social_bot_instagram_register.apps'
+    '.DjangoSocialBotInstagramRegisterConfig',
 ]
 
 MIDDLEWARE = [
